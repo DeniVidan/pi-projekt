026df6160e7193d7169183543abb040ba17ec69a
Deni <denividan@gmail.com>
2022-03-29 15:24:24 +0200

    više button design

--- src/components/Vise.vue
+++ src/components/Vise.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="row more-info" v-if="trenutnaObjava">
-    <button @click="test" class="w3-button">Više</button>
+    <button @click="test" class="w3-button">
+      <div class="vise">Više</div>
+      <div class="cijena">{{ cijena }}kn</div>
+    </button>
     <div :id="trenutnaObjava.id" class="w3-modal">
       <div class="w3-modal-content">
         <div class="w3-container">
@@ -34,6 +37,7 @@ export default {
     toggleLike: Function,
     favorited: Boolean,
     toggleFavorite: Function,
+    cijena: String,
   },
   components: {
     PostOpened,
@@ -65,8 +69,25 @@ export default {
   padding: 0;
 }
 .w3-button {
+  display: flex;
+  flex-direction: row;
   margin-right: 2rem;
   background-color: rgb(187, 187, 187);
+  padding: 0;
+  border-top-right-radius: 10px;
+  border-bottom-right-radius: 10px;
+}
+.cijena {
+  background-color: rgb(211, 211, 211);
+  font-weight: bold;
+  align-self: center;
+  padding: 8px 8px;
+  border-top-right-radius: 10px;
+  border-bottom-right-radius: 10px;
+}
+.vise {
+  padding: 8px 12px;
+  font-weight: bold;
 }
 @media only screen and (max-width: 600px) {
   .w3-button {
